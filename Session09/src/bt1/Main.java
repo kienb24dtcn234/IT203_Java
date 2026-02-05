40c4a1d34f7bc13d6e177909b617dde454d0ce8c
kienb24dtcn234 <thekien780@gmail.com>
2026-02-05 22:43:35 +0700

    Update Main.java

--- Session09/src/bt1/Main.java
+++ Session09/src/bt1/Main.java
@@ -2,7 +2,7 @@ package bt1;
 
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student("Nguyen Quang Huy", 20, "B24DTCN114", 3.6);
+        Student s = new Student("Nguyen Thế Kiên", 20, "B24DTCN232", 4.0);
         s.display();
     }
 }
